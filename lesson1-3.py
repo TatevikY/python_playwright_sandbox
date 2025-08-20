45a938ca0609ac7e200d4b17cfd3dbb11aca5cff
tateviky <tatev.yayloyan@gmail.com>
2025-08-20 16:33:30 +0400

    python playwright sandbox

---
+++ lesson1-3.py
@@ -0,0 +1,18 @@
+# #single comment
+# print("smth")
+# print('smthing')
+# print(5)
+# print('text',5,7,8)
+# #to delete spaces between texts add sep = "" function in the end. always in the end. and if i use sep = "|" then text will be separated by |
+# print('text',5,7,8,sep="")
+# #to print 2 print() in one line use end="" function. if \n is used will be separated into 2 line as in case of default
+# print("first line:", end="")
+# print('second line')
+# print("first line:", end="!!!")
+# print('second line')
+# #special symbols
+# print("text1 \" text2")
+# #other way
+# print('text1 " text2')
+# print("text1 ' text2")
+input()
---
+++ locators.py
@@ -0,0 +1,37 @@
+import re
+from playwright.sync_api import Page, expect
+
+# page.get_by_role() to locate by explicit and implicit accessibility attributes.
+def test_has_title(page: Page):
+    page.goto("https://playwright.dev/")
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+
+# page.get_by_text() to locate by text content.
+def test_has_title(page: Page):
+    page.goto("https://playwright.dev/")
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+
+# page.get_by_label() to locate a form control by associated label's text.
+def test_has_title(page: Page):
+    page.goto("https://playwright.dev/")
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+
+# page.get_by_placeholder() to locate an input by placeholder.
+def test_has_title(page: Page):
+    page.goto("https://playwright.dev/")
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+
+# page.get_by_alt_text() to locate an element, usually image, by its text alternative.
+def test_has_title(page: Page):
+    page.goto("https://playwright.dev/")
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+
+# page.get_by_title() to locate an element by its title attribute.
+def test_has_title(page: Page):
+    page.goto("https://playwright.dev/")
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+
+# page.get_by_test_id() to locate an element based on its data-testid attribute (other attributes can be configured).
+def test_has_title(page: Page):
+    page.goto("https://playwright.dev/")
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
